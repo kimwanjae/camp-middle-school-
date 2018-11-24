b8980178c7a5e84ba8d5c68dda39ff458f72f12d
kimwanjae <37397382+kimwanjae@users.noreply.github.com>
2018-11-24 16:03:09 +0900

    Update README.ml

--- README.ml
+++ README.ml
@@ -1,4 +1,4 @@
-[1. 로봇팔 프로젝트](https://goo.gl/yyKFHm)<br>
+[1. 로봇팔 프로젝트].(https://goo.gl/yyKFHm)<br>
 [2. 무선조정 RC](https://goo.gl)<br>
 [3. Open Source SW & HW](https://goo.gl)<br>
 [4. Open Source SW & HW](https://goo.gl)<br>
